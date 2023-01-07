002c25e0638e20d5a0fbe3de1c1f79417bdb9b81
Maciej Glownia <maciej.glownia@infor.com>
2023-01-07 14:59:09 +0100

    Add Constants with BASE_URLs

---
+++ app/src/main/java/com/glownia/maciej/usersapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.glownia.maciej.usersapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+        const val BASE_URL_TWO = "https://api.dailymotion.com/"
+    }
+}
