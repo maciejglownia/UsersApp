149f1bbe98f5e58d498b0efc960c78ae417ffca5
Maciej Glownia <maciej.glownia@infor.com>
2023-01-07 15:12:47 +0100

    Add UsersRowBinding class with functions to handle click on single row and loading avatar image

---
+++ app/src/main/java/com/glownia/maciej/usersapp/bindingadapters/UsersRowBinding.kt
@@ -0,0 +1,40 @@
+package com.glownia.maciej.usersapp.bindingadapters
+
+import android.util.Log
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
+import coil.load
+import com.glownia.maciej.usersapp.R
+import com.glownia.maciej.usersapp.models.ResultGithub
+import com.glownia.maciej.usersapp.ui.fragments.FirstFragmentDirections
+
+class UsersRowBinding {
+
+    companion object {
+        @BindingAdapter("onUserClickListener")
+        @JvmStatic
+        fun onUserClickListener(singleRow: ConstraintLayout, users: ResultGithub) {
+            Log.d("onUserClickListener", "CALLED")
+            singleRow.setOnClickListener {
+                try {
+                    val action =
+                        FirstFragmentDirections.actionFirstFragmentToSecondFragment(users)
+                    singleRow.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onQuoteClickListener", e.toString())
+                }
+            }
+        }
+
+        @BindingAdapter("loadImageFromUrl")
+        @JvmStatic
+        fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
+            imageView.load(imageUrl) {
+                crossfade(600)
+                error(R.drawable.ic_error_placeholder)
+            }
+        }
+    }
+}
