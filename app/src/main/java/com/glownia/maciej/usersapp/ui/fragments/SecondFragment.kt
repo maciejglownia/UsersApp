a35633718903716ff3d35dc8054f9b7cb7dc3d85
Maciej Glownia <maciej.glownia@infor.com>
2023-01-07 15:12:06 +0100

    Setup SecondFragment where user detail is displaying

--- app/src/main/java/com/glownia/maciej/usersapp/ui/fragments/SecondFragment.kt
+++ app/src/main/java/com/glownia/maciej/usersapp/ui/fragments/SecondFragment.kt
@@ -1,39 +1,42 @@
-package com.glownia.maciej.usersapp
+package com.glownia.maciej.usersapp.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.glownia.maciej.usersapp.databinding.FragmentSecondBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * Displays single user
  */
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
+    private val args by navArgs<SecondFragmentArgs>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
+        setupDetailsRow()
+
         return binding.root
 
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+    // Sets data to display using arguments passed from item has been clicked in the Recycler View
+    private fun setupDetailsRow() {
+        binding.apply {
+            avatarImageView.load(args.user.avatar)
+            usernameTextView.text = args.user.username
+            urlTextView.text = args.user.url
         }
     }
 
