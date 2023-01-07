67f9c77f0e540e20098e0236c2d6f028c17f65a7
Maciej Glownia <maciej.glownia@infor.com>
2023-01-07 15:04:17 +0100

    Add ErrorsBinding class with function to handleReadDataErrors

---
+++ app/src/main/java/com/glownia/maciej/usersapp/bindingadapters/ErrorsBinding.kt
@@ -0,0 +1,33 @@
+package com.glownia.maciej.usersapp.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import com.glownia.maciej.usersapp.data.database.entities.UsersEntity
+import com.glownia.maciej.usersapp.models.UsersGithub
+import com.glownia.maciej.usersapp.utils.NetworkResult
+
+class ErrorsBinding {
+    companion object {
+
+        @BindingAdapter("readApiResponse","readDatabase",requireAll = true)
+        @JvmStatic
+        fun handleReadDataErrors(
+            view: View,
+            apiResponse: NetworkResult<UsersGithub>?,
+            database: List<UsersEntity>?
+        ){
+            when (view){
+                is ImageView ->{
+                    view.isVisible = apiResponse is NetworkResult.Error && database.isNullOrEmpty()
+                }
+                is TextView ->{
+                    view.isVisible = apiResponse is NetworkResult.Error && database.isNullOrEmpty()
+                    view.text = apiResponse?.message.toString()
+                }
+            }
+        }
+    }
+}
