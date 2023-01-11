6df4523ecb021db2e81d480c307d219e65f985bb
Maciej Glownia <maciej.glownia@infor.com>
2023-01-11 07:26:19 +0100

    Add UsersGithubEntity.kt

--- app/src/main/java/com/glownia/maciej/usersapp/data/database/UsersDatabase.kt
+++ app/src/main/java/com/glownia/maciej/usersapp/data/database/UsersDatabase.kt
@@ -3,10 +3,11 @@ package com.glownia.maciej.usersapp.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.glownia.maciej.usersapp.data.database.entities.UsersEntity
+import com.glownia.maciej.usersapp.data.database.entities.UsersDailymotionEntity
+import com.glownia.maciej.usersapp.data.database.entities.UsersGithubEntity
 
 @Database(
-    entities = [UsersEntity::class],
+    entities = [UsersDailymotionEntity::class, UsersGithubEntity::class],
     version = 1,
     exportSchema = false
 )
--- app/src/main/java/com/glownia/maciej/usersapp/data/database/entities/UsersGithubEntity.kt
+++ app/src/main/java/com/glownia/maciej/usersapp/data/database/entities/UsersGithubEntity.kt
@@ -2,14 +2,13 @@ package com.glownia.maciej.usersapp.data.database.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.glownia.maciej.usersapp.models.UsersDailymotion
 import com.glownia.maciej.usersapp.models.UsersGithub
+import com.glownia.maciej.usersapp.utils.Constants.Companion.USERS_GITHUB_TABLE
 
-@Entity(tableName = "users_table")
-class UsersEntity(
-    var usersGithub: UsersGithub,
-    var usersDailymotion: UsersDailymotion
+@Entity(tableName = USERS_GITHUB_TABLE)
+class UsersGithubEntity(
+    var usersGithub: List<UsersGithub>,
 ) {
     @PrimaryKey(autoGenerate = false)
-    var id : Int = 0
+    var id: Int = 0
 }
